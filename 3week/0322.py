127596305b3c85e0eea91dcb98ea8a502931377c
hong <hong@naver.com>
2023-03-22 16:51:35 +0900

    3주차

---
+++ 3week/0322.py
@@ -0,0 +1,42 @@
+'''이것은 
+여러줄
+짜리 
+주석입니다.
+'''
+
+'''print("오늘은 파이썬 수업입니다.")
+print('hello !!')
+print (type("오늘은 파이썬 수업입니다."))
+print (type(2))
+print(5.5)
+
+print("Hello" + "python")
+print("213호" * 3)
+
+print("Hello", "Hi", "213")
+print("hello", 'hello', 213 , "", 11111)
+print("hello" + "python")
+#print("hello" + 23) #에러임
+print("hello" , 23)'''
+
+print(1,2,-5,3.14,2.71828)
+print('Hi', 'python')
+
+print('23000원은', '5000원 ?개', '1000원 ?개')
+print('5000원', 23000 // 5000, '개')
+print('3+8')
+print(3+8)
+print(eval('3+8'))
+
+print(eval('1+62-3*52'))
+print(eval('256*553-152'))
+print(16235290930//539253 + 16235290930%539253)
+a= 78000//50000
+b = 78000%50000
+c = b//10000
+d = c % 10000
+e = d//5000
+
+print('50000원', a, '개')
+print ('10000원은', c, '개')
+print ('5000원은', e, '개')
